ea3279a3e9c6d9d5b5fe90dd09508d107fd1dbb3
Alpha <alpha@pokesplash.net>
2016-12-29 20:01:58 -0500

    New paragraph State and improved transitions

--- docutils/parsers/markdown/states.py
+++ docutils/parsers/markdown/states.py
@@ -18,9 +18,37 @@ def state(cls):
     return cls
 
 
+def indented_lines(lines, indent=0, permitted=' '):
+    """Trims indentation from lines.
+
+    Returns:
+
+    - `lines`: a new trimmed list(str) of lines
+    """
+
+    def trimmed():
+        for line in lines:
+            if line.strip('\t '):
+                # commonmark: @blank-line
+                yield ''
+                continue
+            if not indent:
+                yield line
+                continue
+            # only expand tabs at the start of the line
+            while '\t' in line[:indent]:
+                line = line[:indent].expandtabs(4)+line[indent:]
+            if line[:indent].strip(permitted):
+                return
+    return [line for line in trimmed()]
+
+
 class MarkdownStateMachine(StateMachine):
     """Markdown master StateMachine
     """
+    def __init__(self, state_classes, initial_state, debug=False):
+        StateMachine.__init__(self, state_classes, initial_state, debug)
+
     @classmethod
     def create(cls):
         inst = cls(initial_state='Body', state_classes=state_classes)
@@ -28,28 +56,47 @@ class MarkdownStateMachine(StateMachine):
 
     def run(self, input_lines, input_offset=0, context=None,
             input_source=None, initial_state=None):
+        """Runs this state machine.
+
+        Parameters:
+
+        - `input_lines`: list(str)
+        - `input_offset` : int, optional
+        - `context` : `docutils.nodes.document`
+        - `input_source` : str, optional
+        - `initial_state` : str
+        """
         return StateMachine.run(self, input_lines, input_offset, context,
-                                  input_source, initial_state)
+                                input_source, initial_state)
 
 
 class MarkdownBaseState(State):
-    node_class = docutils.nodes.paragraph
     nested_sm_kwargs = {
         'state_classes': state_classes,
         'initial_state': 'Body',
     }
+    patterns = {
+        'ulist': r'([*+-]) ',
+        'olist': r'(\d+)([.)]) ',
+        'section': r'\s{0,3}(#{1,6})([^#].*)',
+        'fence': r'\s{0,3}(```|~~~)',
+        'thematic_break': r'\s{0,3}([*_-])\s*(\1\s*){2,}$',
+        'paragraph': r'.',
+        'paragraph_end': r'[\t ]*$',
+    }
 
-    def xbof(self, context):
-        if self.node_class is not None:
-            node = self.node_class()
-            context.append(node)
-            context = node
-        return context, []
+    def enter(self, context, next_state, nth=0, indent=0):
+        """Enters a nested statemachine.
 
-    def enter(self, context, next_state):
+        Parameters:
+
+        - `context`: `docutils.nodes.Node`
+        - `next_state` : str
+        """
         substate_machine = self.nested_sm(**self.nested_sm_kwargs)
+        ofs = self.state_machine.line_offset+nth
         results = substate_machine.run(
-            self.state_machine.input_lines[self.state_machine.line_offset+1:],
+            indented_lines(self.state_machine.input_lines[ofs:], indent),
             self.state_machine.abs_line_offset(),
             context=context, initial_state=next_state
         )
@@ -59,35 +106,35 @@ class MarkdownBaseState(State):
 
 @state
 class Section(MarkdownBaseState):
-    node_class = docutils.nodes.section
-    patterns = {
-        'ulist': r'([*+-]) ',
-        'section': r'(#+)([^#].+)',
-        'paragraph': r'.',
-        'new_paragraph': r'',
-    }
     initial_transitions = (
-        'ulist',
+        'thematic_break',
         'section',
+        'ulist',
         'paragraph',
         'new_paragraph',
     )
+
+    def thematic_break(self, match, context, next_state):
+        """Horizontal rule <hr>
+        """
+        node = docutils.nodes.transition()
+        context.append(node)
+        return context, next_state, []
 
     def section(self, match, context, next_state):
         level = match.group(1).count('#')
         if level <= context.level:
             raise EOFError()
-        subcontext = docutils.nodes.section()
-        subcontext.level = level
-        context.append(subcontext)
+        trans_context = context
+        while level > context.level:
+            subcontext = docutils.nodes.section()
+            subcontext.level = level
+            trans_context.append(subcontext)
+            level -= 1
+            trans_context = subcontext
         header = docutils.nodes.title(text=match.group(2).lstrip())
         subcontext.append(header)
-        return context, None, self.enter(subcontext, 'Section')
-
-    def new_paragraph(self, match, context, next_state):
-        node = docutils.nodes.paragraph()
-        context.append(node)
-        return context, next_state, []
+        return context, next_state, self.enter(subcontext, 'Section', nth=1)
 
     def paragraph(self, match, context, next_state):
         """Append text to the last paragraph.
@@ -98,30 +145,23 @@ class Section(MarkdownBaseState):
         paragraph start. (An implicit call to `self.previous_line()` is made
         when correcting transition)
         """
-        if not context.children:
-            self.state_machine.previous_line()
-            raise TransitionCorrection('new_paragraph')
-        last_child = context.children[-1]
-        if not isinstance(last_child, docutils.nodes.paragraph):
-            self.state_machine.previous_line()
-            raise TransitionCorrection('new_paragraph')
-        last_child.append(docutils.nodes.Text(match.string+'\n'))
-        return context, next_state, []
+        node = docutils.nodes.paragraph()
+        context.append(node)
+        return context, next_state, self.enter(node, 'Paragraph')
 
     def ulist(self, match, context, next_state):
         """
         """
         node = docutils.nodes.bullet_list()
         node['bullet'] = match.group(1)
-        if context.children:
-            last_child = context.children[-1]
-            if not last_child.children and isinstance(last_child, docutils.nodes.paragraph):
-                context.replace(last_child, node)
-            else:
-                context.append(node)
-        else:
-            context.append(node)
-        return context, next_state, self.enter(node, 'UList')
+        context.append(node)
+        return context, next_state, self.enter(node, 'ListContainer')
+
+    def olist(self, match, context, next_state):
+        node = docutils.nodes.enumerated_list()
+        node['enumtype'] = 'arabic'
+        context.append(node)
+        return context, next_state, self.enter(node, 'ListContainer')
 
 
 @state
@@ -133,23 +173,22 @@ class Body(Section):
 
 
 @state
-class Text(MarkdownBaseState):
-    node_class = None
-    patterns = {
-        'header': r'(#+)([^#].+)',
-        # 'line': r'([!-/:-@[-`{-~])\1* *$',
-        'text': r''
-    }
+class Paragraph(MarkdownBaseState):
     initial_transitions = (
-        'header',
-        # 'line',
-        'text',
+        'thematic_break',
+        'section',
+        'fence',
+        'paragraph_end',
+        'paragraph',
     )
 
-    def header(self, match, context, next_state):
-        print(match, context, next_state)
-        return context, None, []
+    def paragraph_end(self, match, context, next_state):
+        raise EOFError('End of paragraph')
+    thematic_break = paragraph_end
+    section = paragraph_end
+    fence = paragraph_end
 
-    def text(self, match, context, next_state):
-        """Falls back to default state."""
-        return context, None, []
+    def paragraph(self, match, context, next_state):
+        # TODO: handle inline markup
+        context.append(docutils.nodes.Text(match.string))
+        return context, next_state, []
